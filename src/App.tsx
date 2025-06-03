fc4b5c61e5afb0e19a2b918827c3936eb6cf6235
UzairDevs <uzaircodes0110@gmail.com>
2025-06-03 12:49:37 +0530

    2nd test

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Silver from './components/Silver'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
       <div>
         HEllo pusing stiff to gtihub
       </div>
+      <Silver />
     </>
   )
 }
---
+++ src/components/Silver.tsx
@@ -0,0 +1,8 @@
+export default function Silver(){
+    return (
+
+        <>
+        <div>Hello from Silver. </div>
+        </>
+    )
+}
